9de593b027b74ae6ce825fe2ac984ea27e0521d5
electrodragon <attaullahkhanlodhi@gmail.com>
2020-01-22 17:08:55 +0500

    Test Commit !

--- manage.cpp
+++ manage.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
+int mainScreen();
 
 int main() {
+  mainScreen();
+  return 0;
+}
+
+int mainScreen() {
+  std::cout << "UOS Library Management System" << '\n';
+  std::cout << "\n" << "Press option Key to activate the required function." << '\n';
   
   return 0;
 }
